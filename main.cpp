d0770bf41b71bc3633813e2de8064efec1d82d6e
Rudev <dev.ru@kakao.com>
2023-10-04 22:44:33 +0900

    같은 서브넷 내에서 유니티 클라이언트와 이더넷 통신 성공적.

--- main.cpp
+++ main.cpp
@@ -1,34 +1,115 @@
-#include <wiringPi.h>
-#include <wiringPiSPI.h> // SPI 통신
-#include <iostream> // 표준 입출력
-#include <stdint.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <iostream>
+#include <thread>
 
-#include "Enums.h"
-#include "Types.h"
-#include "JoyStick.h"
-
-using namespace std; // 편하게 입출력을 위한 네임스페이스
-
-int main()
+void udpBroadcast() 
 {
-	wiringPiSetupGpio(); // GPIO 핀번호로 설정 wiringPi 초기화
-	wiringPiSPISetup(SPI_Channel, SPI_Speed); // SPI wiringPi라이브러리 SPI 초기화
-	pinMode(GPN_SPIChannel, OUTPUT); // MCP_3008의 CS핀을 출력 모드로 설정
+    int udpSock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (udpSock < 0) 
+    {
+        std::cerr << "Error creating UDP socket\n";
+        return;
+    }
 
-	JoyStick stick(ModuleType::MovingJoyStick);
-	stick.Init();
-	
-	while (true)
-	{
-		bool buttonInput = stick.GetInputValue();
-		Coordinate coord = stick.GetCoordinate();
+    int broadcastEnable = 1;
+    int ret = setsockopt(udpSock, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable));
+    if (ret) 
+    {
+        std::cerr << "Error setting socket options\n";
+        return;
+    }
 
-		cout << "button pressed: " << (buttonInput ? "true" : "false")
-			<< ", x좌표: " << coord.x
-			<< ", y좌표: " << coord.y << endl;
+    struct sockaddr_in broadcastAddr;
+    memset(&broadcastAddr, 0, sizeof(broadcastAddr));
+    broadcastAddr.sin_family = AF_INET;
+    broadcastAddr.sin_addr.s_addr = htonl(INADDR_BROADCAST);
+    broadcastAddr.sin_port = htons(12345);
 
-		delay(100);
-	}
+    while (true) 
+    {
+        const char* broadcastMessage = "Hello, Unity!";
+        ret = sendto(udpSock, broadcastMessage, strlen(broadcastMessage), 0, (struct sockaddr*)&broadcastAddr, sizeof(broadcastAddr));
+        if (ret < 0) 
+        {
+            std::cerr << "Error sending UDP broadcast\n";
+            break;
+        }
 
-	return 0;
+        usleep(100000); // 100ms delay
+    }
+
+    close(udpSock);
+}
+
+void acceptTcpConnection() 
+{
+    int tcpSock = socket(AF_INET, SOCK_STREAM, 0);
+    if (tcpSock < 0) 
+    {
+        std::cerr << "Error creating TCP socket\n";
+        return;
+    }
+
+    struct sockaddr_in serverAddr;
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddr.sin_port = htons(23456);
+
+    if (bind(tcpSock, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) 
+    {
+        std::cerr << "Error binding TCP socket\n";
+        return;
+    }
+
+    if (listen(tcpSock, 1) < 0) 
+    {
+        std::cerr << "Error listening on TCP socket\n";
+        return;
+    }
+
+    while (true) 
+    {
+        struct sockaddr_in clientAddr;
+        socklen_t clientAddrLen = sizeof(clientAddr);
+        int clientSock = accept(tcpSock, (struct sockaddr*)&clientAddr, &clientAddrLen);
+        if (clientSock < 0) {
+            std::cerr << "Error accepting TCP connection\n";
+            break;
+        }
+
+        // TCP로 계속 데이터 전송
+        while (true) 
+        {
+            int32_t data = 123; // 보낼 데이터
+            int ret = send(clientSock, &data, sizeof(data), 0);
+            if (ret < 0) 
+            {
+                std::cerr << "Error sending data\n";
+                break;
+            }
+            
+            // 필요에 따라 sleep 등을 사용하여 전송 간격을 조절할 수 있습니다.
+            // sleep(1);
+        }
+
+        close(clientSock);
+    }
+
+    close(tcpSock);
+}
+
+int main() 
+{
+    std::thread udpThread(udpBroadcast);
+    std::thread tcpThread(acceptTcpConnection);
+
+    udpThread.join();
+    tcpThread.join();
+
+    return 0;
 }
